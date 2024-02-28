e6a6adfff16f191103eaea6037e7ce1d2392e2e1
Pouya Faridi <pouya.faridi@outlook.com>
2024-02-28 05:53:44 +0300

    Todo

--- Project/Dariosoft.gRPCTool.V2/Composers/ProtobufMessageComponentComposer.cs
+++ Project/Dariosoft.gRPCTool.V2/Composers/ProtobufMessageComponentComposer.cs
@@ -4,7 +4,10 @@ namespace Dariosoft.gRPCTool.V2.Composers
     {
         protected override void Process(Components.ProtobufComponent component)
         {
-            
+            //TODO: Generate message and push to ProtobufModel.Messages
+            //1: Something like a recursive function must resolve all the complex messages and reply messages
+            //2: start from request message parameters type
+            //3: Then reply message parameter type
         }
     }
 }
